9a2ec86628113fedb8f2853825ee959be2527af5
grafgustav <grafgustav@rambler.ru>
2025-04-04 17:52:26 -0500

    added spring backend

---
+++ backend/src/main/kotlin/com/guero/parent_tinder/ParentTinderApplication.kt
@@ -0,0 +1,11 @@
+package com.guero.parent_tinder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ParentTinderApplication
+
+fun main(args: Array<String>) {
+	runApplication<ParentTinderApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/guero/parent_tinder/ParentTinderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.guero.parent_tinder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ParentTinderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
